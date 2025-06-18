0187ae6d51cc6c956351fca55619d82a4f3ff03c
Kushi Numdin <numdinkushi@gmail.com>
2025-06-18 11:25:23 +0100

    build clean

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,12 +4,12 @@ import dynamic from "next/dynamic";
 
 
 // note: dynamic import is required for components that use the Frame SDK
-const Demo = dynamic(() => import("~/components/Main"), {
+const Main = dynamic(() => import("~/components/Main"), {
   ssr: false,
 });
 
 export default function App(
-  { title }: { title?: string } = { title: process.env.NEXT_PUBLIC_FRAME_NAME || "Frames v2 Demo" }
+  // { title }: { title?: string } = { title: process.env.NEXT_PUBLIC_FRAME_NAME || "Frames v2 Main" }
 ) {
-  return <Demo title={title} />;
+  return <Main  />;
 }
