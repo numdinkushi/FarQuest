93e4bbc81e516e34d64b382a402a1fb95bcfd705
Kushi Numdin <numdinkushi@gmail.com>
2025-06-20 11:53:24 +0100

    build clean up

--- src/hooks/use-game/use-wallet.tsx
+++ src/hooks/use-game/use-wallet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useState, useCallback, useEffect } from 'react';
 import { useAccount, useDisconnect, useConnect, useSwitchChain, useChainId } from 'wagmi';
 import { WalletState } from '~/types';
